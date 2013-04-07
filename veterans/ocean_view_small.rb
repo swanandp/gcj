18d1817778e61cc0dca28eccc294e3e86bd0609a
Swanand Pagnis <swanand.pagnis@gmail.com>
2013-04-07 16:39:21 +0530

    Added ocean view solutions (large fails right now - longest subsequence algo is not correct)

---
+++ veterans/ocean_view_small.rb
@@ -0,0 +1,25 @@
+# ocean_view
+count = 0
+case_no = 1
+current_count = nil
+f = File.open('ocean_view.large.txt', 'wb')
+ARGF.each do |line|
+  count += 1
+  next if count == 1 || count % 2 == 0
+  heights = line.strip.split(' ').map(&:to_i)
+  differences = []
+  heights.each_with_index do |h, i|
+    next if i == heights.length - 1
+    differences << heights[i + 1] - h
+  end
+  destroyed = 0
+  differences.each_with_index do |d, i|
+    if d <= 0
+      destroyed += 1
+      differences[i + 1] += d if differences[i + 1]
+    end
+  end
+  f.puts "Case ##{case_no}: #{destroyed}"
+  case_no += 1
+end
+f.close
