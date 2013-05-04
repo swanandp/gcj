51a1120f6d8c16548b4e87986473c4529ec2b80e
Swanand Pagnis <swanand.pagnis@gmail.com>
2013-05-04 14:38:38 +0530

    Added balanced edge shortest path and some tests for it

---
+++ practice/shortest_path.rb
@@ -0,0 +1,62 @@
+require 'priority_queue'
+
+class Graph
+  attr_accessor :vertices, :distances, :previous, :start, :finish
+  def initialize
+    @vertices = {}
+  end
+
+  def add_vertex(vertex, edges)
+    @vertices[vertex] = edges
+  end
+
+  def shortest_path(start, finish)
+    maxint = 1.0 / 0
+    @start = start
+    @finish = finish
+    @distances = {}
+    @previous = {}
+    nodes = PriorityQueue.new
+
+    @vertices.each do |vertex, edges|
+      if vertex == @start
+        @distances[vertex] = 0
+        nodes[vertex] = 0
+      else
+        @distances[vertex] = maxint
+        nodes[vertex] = maxint
+      end
+      @previous[vertex] = nil
+    end
+
+    while nodes.length > 0
+      smallest = nodes.delete_min_return_key
+      
+      # nothing left or all infinite edges
+      break if smallest == nil or @distances[smallest] == maxint
+      
+      @vertices[smallest].each do | neighbor, value |
+        alt = @distances[smallest] + value
+        if alt < @distances[neighbor]
+          @distances[neighbor] = alt
+          @previous[neighbor] = smallest
+          nodes[neighbor] = alt
+        end
+      end
+    end
+  end
+  
+  def path
+    path = []
+    current = @finish
+    while @previous[current]
+      path.push(current)
+      current = @previous[current]
+    end
+    path.push(@start).reverse
+  end
+  
+  def distance
+    @distances[@finish]
+  end
+end
---
+++ practice/shortest_path_test.rb
@@ -0,0 +1,49 @@
+$:.unshift File.dirname(__FILE__)
+require 'shortest_path'
+require 'minitest/autorun'
+
+class ShortestPath < MiniTest::Unit::TestCase
+  def setup
+    @graph = Graph.new
+    @graph.add_vertex('A', 'B' => 7, 'C' => 8)
+    @graph.add_vertex('B', 'A' => 7, 'F' => 2)
+    @graph.add_vertex('C', 'A' => 8, 'F' => 6, 'G' => 4)
+    @graph.add_vertex('D', 'F' => 8)
+    @graph.add_vertex('E', 'H' => 1)
+    @graph.add_vertex('F', 'B' => 2, 'C' => 6, 'D' => 8, 'G' => 9, 'H' => 3)
+    @graph.add_vertex('G', 'C' => 4, 'F' => 9)
+    @graph.add_vertex('H', 'E' => 1, 'F' => 3)
+  end
+
+  def test_shortest_a_to_h
+    @graph.shortest_path('A', 'E')
+    assert_equal ['A', 'B', 'F', 'H', 'E'], @graph.path
+  end
+
+  def test_shortest_a_to_h_distance
+    @graph.shortest_path('A', 'E')
+    assert_equal 13, @graph.distance
+  end
+
+  def test_shortest_c_to_a
+    @graph.shortest_path('C', 'A')
+    assert_equal ['C', 'A'], @graph.path
+  end
+
+  def test_shortest_c_to_a_distance
+    @graph.shortest_path('C', 'A')
+    assert_equal 8, @graph.distance
+  end
+
+  def test_shortest_a_to_g
+    @graph.shortest_path('A', 'G')
+    assert_equal ['A', 'C', 'G'], @graph.path
+  end
+
+  def test_shortest_a_to_g_distance
+    @graph.shortest_path('A', 'G')
+    assert_equal 12, @graph.distance
+  end
+
+
+end
